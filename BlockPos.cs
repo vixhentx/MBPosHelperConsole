4e7142432d18fa5eb98715492d5ebf480f60b4c7
淼涛 温 <w1084349470@outlook.com>
2025-05-31 22:58:25 +0800

    .vs/ 	bin/ 	obj/

---
+++ BlockPos.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MBPosHelperConsole
+{
+    public class BlockPos
+    {
+        public int X;
+        public int Y;
+        public int Z;
+        public BlockPos() { }
+        public BlockPos(int x, int y, int z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+        public BlockPos Copy() => new BlockPos(X, Y, Z);
+        public BlockPos Subtract(BlockPos right)
+        {
+            X -= right.X;
+            Y -= right.Y;
+            Z -= right.Z;
+            return this;
+        }
+        public static BlockPos operator+ (BlockPos left, BlockPos right) => new BlockPos(left.X + right.X, left.Y + right.Y, left.Z + right.Z);
+        public static BlockPos operator- (BlockPos left, BlockPos right) => new BlockPos(left.X - right.X, left.Y - right.Y, left.Z - right.Z);
+    }
+}
---
+++ GTPattern.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MBPosHelperConsole
+{
+    public class GTPattern
+    {
+        //[x][y][z]:
+        // x-为机器左边，y-为机器下面，z-为机器后面
+        char[,,] symbol = new char[256,256,256];
+        Dictionary<char, List<BlockPos>> map = [];
+        public char this[int x, int y, int z]
+        {
+            get => symbol[x, y, z];
+            set
+            {
+                List<BlockPos>? list;
+                if (!map.TryGetValue(value, out list) || list == null)
+                {
+                    list = new();
+                    map.Add(value, list);
+                }
+                list.Add(new(x, y, z));
+            }
+        }
+        public List<BlockPos> this[char symbol] => map[symbol];
+        public int SizeX {get;set;}
+        public int SizeY {get;set;}
+        public int SizeZ {get;set;}
+
+        public GTPattern(string pattern)
+        {
+            // 匹配 aisle(...)
+            Regex callRegex = new Regex(
+                pattern:
+                @"
+                    \.aisle\s*        
+                    (\(                 
+                    (?:                 
+                    (?:                 
+                        ""(?:\\""|[^""])*"" 
+                        | [^()]         
+                    )*                  
+                    )                   
+                    \))
+                ",               
+                options:
+                RegexOptions.IgnorePatternWhitespace
+            );
+
+            MatchCollection callMatches = callRegex.Matches(pattern);
+            SizeZ = callMatches.Count;
+
+            int z = 0;
+            foreach (Match callMatch in callMatches)
+            {
+                if (callMatch.Success)
+                {
+                    string args = callMatch.Groups[1].Value;
+                    Regex stringRegex = new Regex(
+                        pattern:
+                        @"
+                            ""               
+                            (                  
+                                (?:            
+                                    \\""       
+                                    | [^""]    
+                                )*             
+                            )                  
+                            ""
+                        ",
+                        options:
+                        RegexOptions.IgnorePatternWhitespace
+                    );
+
+                    MatchCollection argMatches = stringRegex.Matches(args);
+                    int y = 0;
+                    foreach (Match argMatch in argMatches)
+                    {
+                        if (argMatch.Success)
+                        {
+                            // 处理转义
+                            string cleaned = argMatch.Groups[1].Value
+                                .Replace("\\\"", "\"")   
+                                .Replace("\\\\", "\\");
+
+                            int x = 0;
+                            foreach (char c in cleaned) 
+                            {
+                                this[x,y,z] = c;
+                                x++;
+                            }
+                            SizeX = x;
+                            y++;
+                        }
+                    }
+                    SizeY = y;
+
+                    z++;
+                }
+                //else throw new InvalidCastException("Failed to match");
+            }
+
+            BlockPos controllerPos = this['@'][0];
+            foreach(var vList in map.Values)
+                foreach(var pos in vList)
+                    pos.Subtract(controllerPos);
+        }
+    }
+}
