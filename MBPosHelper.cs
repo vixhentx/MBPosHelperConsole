0413d04a3eee3dfa79f012685af0514165554ae0
淼涛 温 <w1084349470@outlook.com>
2025-06-01 09:41:20 +0800

    ReadLine+CommandLineParser

---
+++ MBPosHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MBPosHelperConsole
+{
+    public class MBPosHelper
+    {
+        GTPattern? pattern;
+        public void Pattern(string )
+        {
+
+        }
+    }
+}
